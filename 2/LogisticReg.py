a38c1a285d4d185f28766d603c61d48c5aee4239
Yujin-stella <Yujin-stella@users.noreply.github.com>
2020-07-23 17:25:31 +0900

    homework

---
+++ 2/LogisticReg.py
@@ -0,0 +1,95 @@
+﻿
+import tensorflow as tf
+import numpy as np
+
+# [털, 날개] , [기타, 포유류, 조류] 데이터20개
+x_data = np.array(
+    [[0, 0], [1, 0], [1, 1], [0, 0], [0, 0], [0, 1],[0, 0], [1, 0], [1, 1], [0, 0], [0, 0], [0, 1],[1, 0], [1, 1],	[0, 0], [1, 0], [1, 1], [0, 0], [0, 0], [0, 1]])
+
+y_data = np.array([
+    [1, 0, 0],  # 기타
+    [0, 1, 0],  # 포유류
+    [0, 0, 1],  # 조류
+    [1, 0, 0],
+    [1, 0, 0],
+    [0, 0, 1],
+    [1, 0, 0],  
+    [0, 1, 0],  
+    [0, 0, 1],  
+    [1, 0, 0],
+    [1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0],  
+    [0, 0, 1], 
+    [1, 0, 0],  
+    [0, 1, 0],  
+    [0, 0, 1],  
+    [1, 0, 0],
+    [1, 0, 0],
+    [0, 0, 1]
+])
+
+# 신경망 모델 구성
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+#[입력-2 , 히든레이어1-10]
+W1 = tf.Variable(tf.random_uniform([2, 10], -1., 1.))
+#[ 히든레이어1-10, 히든레이어2-20]
+W2 = tf.Variable(tf.random_uniform([10, 20], -1., 1.))
+#[ 히든레이어2-20, 히든레이어3-10]
+W3 = tf.Variable(tf.random_uniform([20, 10], -1., 1.))
+#[ 히든레이어3-10, 출력-3]
+W4 = tf.Variable(tf.random_uniform([10, 3], -1., 1.))
+
+
+# 편향
+b1 = tf.Variable(tf.zeros([10]))
+b2 = tf.Variable(tf.zeros([20]))
+b3 = tf.Variable(tf.zeros([10]))
+b4 = tf.Variable(tf.zeros([3]))
+
+# 신경망의 히든 레이어에 가중치 W1과 편향 b1을 적용합니다
+L1 = tf.add(tf.matmul(X, W1), b1)
+L1 = tf.nn.relu(L1)
+
+L2 = tf.add(tf.matmul(L1, W2), b2)
+L2 = tf.nn.relu(L2)
+
+L3 = tf.add(tf.matmul(L2, W3), b3)
+L3 = tf.nn.relu(L3)
+
+model = tf.add(tf.matmul(L3, W4), b4)
+
+#비용 함수
+cost = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=model))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost)
+
+
+
+# 신경망 모델 학습
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+for step in range(100):
+    sess.run(train_op, feed_dict={X: x_data, Y: y_data})
+
+    if (step + 1) % 10 == 0:
+        print(step + 1, sess.run(cost, feed_dict={X: x_data, Y: y_data}))
+
+
+# 결과 확인
+
+prediction = tf.argmax(model, 1)
+target = tf.argmax(Y, 1)
+print('예측값:', sess.run(prediction, feed_dict={X: x_data}))
+print('실제값:', sess.run(target, feed_dict={Y: y_data}))
+
+is_correct = tf.equal(prediction, target)
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+print('정확도: %.2f' % sess.run(accuracy * 100, feed_dict={X: x_data, Y: y_data}))
---
+++ 3/MINIST.py
@@ -0,0 +1,89 @@
+﻿
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+
+
+# 신경망 모델 구성
+#입력784 -> 은닉256 -> 은닉256 -> 은닉256 -> 출력10
+
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)	#드롭아웃 수치
+
+W1 = tf.Variable(tf.random_normal([784, 256], stddev=0.01))
+L1 = tf.nn.relu(tf.matmul(X, W1))
+L1 = tf.nn.dropout(L1, keep_prob)	#드롭아웃
+
+W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
+L2 = tf.nn.relu(tf.matmul(L1, W2))
+L2 = tf.nn.dropout(L2, keep_prob)
+
+W3 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
+L3 = tf.nn.relu(tf.matmul(L2, W3))
+L3 = tf.nn.dropout(L3, keep_prob)
+
+W4 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
+model = tf.matmul(L3, W4)	#최종출력
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))	#cross entropy & softmax 적용
+optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)		#adamOprimizer이용
+
+
+# 신경망 모델 학습
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+batch_size = 100		#배치 사이즈
+total_batch = int(mnist.train.num_examples / batch_size)	#배치 개수
+
+for epoch in range(30):
+    total_cost = 0
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+
+        _, cost_val = sess.run([optimizer, cost],
+                               feed_dict={X: batch_xs,
+                                          Y: batch_ys,
+                                          keep_prob: 0.8})		#뉴런의 이용 퍼센트 
+        total_cost += cost_val
+
+    print('Epoch:', '%04d' % (epoch + 1),
+          'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))		#에폭마다 cost출력
+
+print('최적화 완료!')
+
+
+# 결과 확인
+
+is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+print('정확도:', sess.run(accuracy,
+                        feed_dict={X: mnist.test.images,
+                                   Y: mnist.test.labels,
+                                   keep_prob: 1}))		#test에서는 뉴런 100퍼센트 사용
+
+
+# 결과 확인 (matplot)
+
+labels = sess.run(model,
+                  feed_dict={X: mnist.test.images,
+                             Y: mnist.test.labels,
+                             keep_prob: 1})
+
+fig = plt.figure()
+for i in range(10):
+    subplot = fig.add_subplot(2, 5, i + 1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.set_title('%d' % np.argmax(labels[i]))
+    subplot.imshow(mnist.test.images[i].reshape((28, 28)),
+                   cmap=plt.cm.gray_r)
+
+plt.show()
